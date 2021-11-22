16d9320fc3ce3c920a9f76073e107b1197e417f7
Anas Abid <abid.anas3@gmail.com>
2021-11-23 02:15:46 +0500

    practiceee

---
+++ app.js
@@ -0,0 +1,33 @@
+// alert("ERROR!")
+
+// function remove(el) {
+//     var element = el;
+//     element.remove();
+//   }
+
+
+// function myImg2(){
+
+//     var myImg = document.getElementById("myImg")
+//     myImg.src = "./my img 2.jpg"
+
+
+// }
+
+// function myImg(){
+
+//     var myImg = document.getElementById("myImg")
+//     myImg.src = "./my img.jpg"
+
+
+// }
+
+
+function seeMore(){
+
+    var para = document.getElementById("para1")
+    var showPara = document.getElementById("showPara")
+    para.innerHTML  = showPara.innerHTML
+
+
+}
