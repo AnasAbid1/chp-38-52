ff7d17763043f6cfbb7aa3da1069c756db16256a
Anas Abid <92758772+AnasAbid1@users.noreply.github.com>
2021-11-30 00:19:58 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 // alert("ERROR!")
 
+// chp38-42
+// FUNCTIONS, SWITCH STATEMENTS, WHILE... DO-WHILE LOOPS
+
+
+//1)
+// Math.pow(not studied)
+
+//2)
+// ---------------
+
+
+//3)
+
+///////////////////////////////////////////////
+
 // function remove(el) {
 //     var element = el;
 //     element.remove();
@@ -23,11 +38,83 @@
 // }
 
 
-function seeMore(){
+// function seeMore(){
 
-    var para = document.getElementById("para1")
-    var showPara = document.getElementById("showPara")
-    para.innerHTML  = showPara.innerHTML
+//     var para = document.getElementById("para1")
+//     var showPara = document.getElementById("showPara")
+//     para.innerHTML  = showPara.innerHTML
 
 
-}
+// }
+
+                                   // MOUSEOVER BULB //
+
+// function offBulb(){
+
+//     var myImg = document.getElementById("imgoff")
+//     myImg.src = "./off.gif"
+
+
+// }
+
+// function bulbImg(){
+
+//     var myImg = document.getElementById("imgoff")
+//     myImg.src = "./on.gif"
+
+
+// }
+
+
+                    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+                                //heading colour change//
+
+
+//  var heading = document.getElementsByTagName("h3");
+
+//     console.log(heading)
+
+//     var headFirst = prompt("ENTER FIRST HEADING COLOUR")
+//     var headSecond = prompt("ENTER SECOND HEADING COLOUR")
+
+// for(var i = 0 ; i < heading.length ; i++){
+
+//     heading[i].style.color = headFirst
+//     heading[i].style.color = headSecond
+
+// }
+
+                                //BIG,SMALL,SHOW AND HIDE
+
+// function hide(){
+        
+//         var carImg = document.getElementById("myImg");
+//         console.log(myImg.className)
+//         carImg.className = "hidden"
+// }
+
+// function imgControl (imgClass){
+
+//     var carImg = document.getElementById("myImg");
+//     myImg.className += " bigImg"
+// }
+
+                                //PARA COLOUR
+
+//      var paras = document.getElementsByTagName("p");
+
+//     console.log(paras)
+
+//     var parColour = prompt("CHOOSE A COLOUR FOR THE TEXT BELOW:")
+
+// for(var i = 0 ; i < paras.length ; i++){
+//     paras[i].style.color = parColour
+
+// }
+// var spParas = fourParas.getElementsByTagName("p")
+// console.log(spParas)
